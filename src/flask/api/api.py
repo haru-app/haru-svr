d4540c09fca957e8c00f3d454a9d7c75b93dbd79
sdg9670 <sdg9670@naver.com>
2020-08-23 21:32:37 +0900

    코드 추가

--- src/flask/api/api.py
+++ src/flask/api/api.py
@@ -1,14 +1,16 @@
 from src.flask.api.auth.controller.register import RegisterController
 from src.flask.api.auth.controller.token import TokenController
 from src.flask.api.auth.controller.login import LoginController
+from .code.controller.all import AllCodeController
 from .user.controller.email_duplicate import EmailDuplicateController
 
 
 class Api:
     def getRouters(self):
         return [
-            {'class': EmailDuplicateController, 'path': '/api/user/email/duplicate'},
             {'class': LoginController, 'path': '/api/auth/login'},
             {'class': RegisterController, 'path': '/api/auth/register'},
-            {'class': TokenController, 'path': '/api/auth/token'}
+            {'class': TokenController, 'path': '/api/auth/token'},
+            {'class': EmailDuplicateController, 'path': '/api/user/email/duplicate'},
+            {'class': AllCodeController, 'path': '/api/code/all'}
         ]
